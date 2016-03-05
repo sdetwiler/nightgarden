a4e5882615e2cfe28022bf3829d4fb51043a56c0
Steve Detwiler <steve@n3twork.com>
2016-03-05 10:23:12 -0800

    Do not regenerate meshes every frame. Only regenerate after a new LSystem step.

--- src/ofApp.cpp
+++ src/ofApp.cpp
@@ -4,8 +4,7 @@
 
 #include <stack>
 
-typedef std::stack<ofMatrix4x4>MatrixStack;
-
+typedef std::stack<ofMatrix4x4> MatrixStack;
 
 
 
@@ -18,7 +17,7 @@ void ofApp::setup(){
 		mCurrSteps = 0;
 		mMaxSteps = 14;
 		mLastStepTime = 0;
-		mStepInterval = 0;
+		mStepInterval = 1;
 	}
 	
 	r=0;
@@ -38,44 +37,64 @@ void ofApp::update()
 
 			SymbolList const* state = LSystem::getInstance().getState();
 //			std::cout << state->toString() << std::endl;
+			
+			buildMeshes();
+
 		}
 	}
 	
-	r+=1;
+//	r+=1;
 	if(r>360)
 	{
 		r = 0;
 	}
 }
 
-//--------------------------------------------------------------
-void ofApp::draw()
+
+void ofApp::drawMeshes()
 {
-	// HACK for stable color generation below...
-	ofSeedRandom(1);
+	for(MeshVec::iterator i = mMeshes.begin(); i!=mMeshes.end(); ++i)
+	{
+		(*i)->drawWireframe();
+//		(*i)->draw();
+	}
+}
 
-	ofEnableDepthTest();
+void ofApp::clearMeshes()
+{
+	for(MeshVec::iterator i = mMeshes.begin(); i!=mMeshes.end(); ++i)
+	{
+		delete(*i);
+	}
 	
-//	std::cout << "==================================" << std::endl;
-	VariableMap const& globals = LSystem::getInstance().getGlobalVariables();
+	mMeshes.clear();
+}
 
+
+void ofApp::buildMeshes()
+{
+	clearMeshes();
+	
+	//	std::cout << "==================================" << std::endl;
+	VariableMap const& globals = LSystem::getInstance().getGlobalVariables();
+	
 	float delta = 22.5;
 	StringVariableMap::const_iterator i;
-
+	
 	i = globals.variables.find(std::string("delta"));
 	if(i!=globals.variables.end())
 	{
 		delta = i->second->value;
 	}
 	
-
+	
 	int n = 5;
 	i = globals.variables.find(std::string("n"));
 	if(i!=globals.variables.end())
 	{
 		n = i->second->value;
 	}
-
+	
 	int w = ofGetWidth();
 	int h = ofGetHeight();
 	
@@ -83,45 +102,46 @@ void ofApp::draw()
 	float ty = h*0.75;
 	// Match openframeworks rendering context.
 	delta*=-1;
-
-	MatrixStack matrixStack;
-
 	
-
+	MatrixStack matrixStack;
+	
+	
+	
 	ofMatrix4x4 rm;
 	rm.glTranslate(tx, ty, 0);
 	rm.glRotate(180,0,0,1);
-
+	
 	rm.glRotate(r,0,1,0);
 	
 	matrixStack.push(rm);
 	ofMatrix4x4 currMatrix = matrixStack.top();
-
-
+	
 	
 	SymbolList const* state = LSystem::getInstance().getState();
-
+	
 	if(state)
 	{
+		ofMesh* currMesh = nullptr;
 		ofMesh* poly = nullptr;
 		ofColor polyColor(0,180,0);
-
+		
 		for(SymbolVec::const_iterator i = state->symbols.begin(); i!=state->symbols.end(); ++i)
 		{
 			
-//			std::cout << "Matrix Audit\n------------\nglMatrix\n" << ofGetCurrentMatrix(OF_MATRIX_MODELVIEW) << "\nmyMatrix\n" << currMatrix << "\n\n";
+			//			std::cout << "Matrix Audit\n------------\nglMatrix\n" << ofGetCurrentMatrix(OF_MATRIX_MODELVIEW) << "\nmyMatrix\n" << currMatrix << "\n\n";
 			
 			
 			Symbol* s = *i;
 			if(s->value == "F")
 			{
-				ofMesh mesh;
+				// Next try joining to existing mesh...
+				currMesh = new ofMesh();
 				
 				float r = 1.0;
 				int sides = 4;
 				
 				float dr = 360.0/sides;
-
+				
 				ofColor brown(139,69,19);
 				brown = brown * ofRandom(0.5, 1.5);
 				
@@ -137,18 +157,19 @@ void ofApp::draw()
 					v1 = v1 * currMatrix;
 					v2 = v2 * currMatrix;
 					
-					mesh.addVertex(v1);
-					mesh.addColor(brown);
-
-					mesh.addVertex(v2);
-					mesh.addColor(brown);
+					currMesh->addVertex(v1);
+					currMesh->addColor(brown);
+					
+					currMesh->addVertex(v2);
+					currMesh->addColor(brown);
 				}
-				mesh.setMode(OF_PRIMITIVE_TRIANGLE_STRIP);
-				mesh.draw();
+				currMesh->setMode(OF_PRIMITIVE_TRIANGLE_STRIP);
+				mMeshes.push_back(currMesh);
+				currMesh = nullptr;
 				
 				currMatrix.glTranslate(0,n,0);
 			}
-
+			
 			if(s->value == "G")
 			{
 				currMatrix.glTranslate(0,n,0);
@@ -160,19 +181,19 @@ void ofApp::draw()
 				{
 					ofVec4f v1(0,0,0,1);
 					v1 = v1 * currMatrix;
-//					std::cout << v1 << std::endl;
+					//					std::cout << v1 << std::endl;
 					poly->addVertex(v1);
 					poly->addColor(polyColor);
 				}
 			}
-
+			
 			else if(s->value == "f")
 			{
 				if(poly)
 				{
 					ofVec4f v1(0,0,0,1);
 					v1 = v1 * currMatrix;
-//					std::cout << v1 << std::endl;
+					//					std::cout << v1 << std::endl;
 					poly->addVertex(v1);
 					poly->addColor(polyColor);
 				}
@@ -182,13 +203,14 @@ void ofApp::draw()
 			{
 				if(poly)
 				{
+					std::cout << "Warning: New poly started while an existing poly was being built. Deleting existing poly.\n";
 					delete poly;
 				}
 				
 				poly = new ofMesh();
 				if(s->expressions && s->expressions->expressions.size() == 3)
 				{
-//					std::cout << s->expressions->toString() << std::endl;
+					//					std::cout << s->expressions->toString() << std::endl;
 					polyColor.r = stoi((*(s->expressions->expressions[0])).value);
 					polyColor.g = stoi((*(s->expressions->expressions[1])).value);
 					polyColor.b = stoi((*(s->expressions->expressions[2])).value);
@@ -199,13 +221,12 @@ void ofApp::draw()
 			{
 				if(poly)
 				{
-					poly->setMode(OF_PRIMITIVE_TRIANGLE_FAN); // SCD HACK This is actually wrong
-					poly->draw();
-					delete poly;
+					poly->setMode(OF_PRIMITIVE_TRIANGLE_FAN);
+					mMeshes.push_back(poly);
 					poly = nullptr;
 				}
 			}
-
+			
 			else if(s->value == "[")
 			{
 				matrixStack.push(currMatrix);
@@ -246,6 +267,14 @@ void ofApp::draw()
 			}
 		}
 	}
+}
+
+
+//--------------------------------------------------------------
+void ofApp::draw()
+{
+	ofEnableDepthTest();
+	drawMeshes();
 }
 
 //--------------------------------------------------------------
